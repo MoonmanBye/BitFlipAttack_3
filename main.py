638aade894700141277ad58ce13120e22a65c226
ZiyuLiu <49537782+MoonmanBye@users.noreply.github.com>
2022-12-20 23:11:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ else:
     device = torch.device('cuda')
     print('Using gpu: ' + args.gpu)
 
-def generateNoise(layer_weight)：
+def generateNoise(layer_weight):
     #ori_conv1_grad = model.module.conv1.weight.grad.clone()
     ori_grad =layer_weight.clone()
     var_grad=(torch.var(ori_grad, unbiased=False))
